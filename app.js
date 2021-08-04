ecf547ec071fc2afc38abe20acb205bc87b8052d
mdabrarzahinantor <mdabrarzahinantor2005@gmail.com>
2021-08-04 10:57:23 +0600

    init

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-var userInput;
-// let userName:string;
-userInput = 5;
-userInput = "Max";
-// userName=userInput;
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var last = function (arr) {
+    return arr[arr.length - 1];
+};
+var l = last([1, 2, 3]);
+var l2 = last(["a", "b", "c"]);
+var makeArr = function (x, y) {
+    return [x, y];
+};
+var v = makeArr(5, 6);
+var v2 = makeArr("a", "b");
+var v3 = makeArr("a", 1);
+var makeFullname = function (obj) {
+    return __assign(__assign({}, obj), { fullName: obj.firstName + " " + obj.lastName });
+};
+var v4 = makeFullname({ firstName: "bob", lastName: "junior", age: 15 });
--- app.ts
+++ app.ts
@@ -1,6 +1,33 @@
-let userInput: unknown;
-// let userName:string;
+const last = <T>(arr: T[]): T => {
+  return arr[arr.length - 1];
+};
 
-userInput = 5;
-userInput = "Max";
-// userName=userInput;
+const l = last([1, 2, 3]);
+const l2 = last<string>(["a", "b", "c"]);
+
+const makeArr = <X, Y = number>(x: X, y: Y): [X, Y] => {
+  return [x, y];
+};
+
+const v = makeArr(5, 6);
+const v2 = makeArr("a", "b");
+const v3 = makeArr<string | null>("a", 1);
+
+const makeFullname = <T extends { firstName: string; lastName: string }>(
+  obj: T
+) => {
+  return {
+    ...obj,
+    fullName: obj.firstName + " " + obj.lastName,
+  };
+};
+
+const v4 = makeFullname({ firstName: "bob", lastName: "junior", age: 15 });
+
+interface Tab<T> {
+  id: string;
+  position: number;
+  data: T;
+}
+type NumberTab = Tab<number>;
+type StrtingTab = Tab<string>;
